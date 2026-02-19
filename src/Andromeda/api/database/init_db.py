1d042cd45aed7781326431188846dd90750dfae1
Jachym <68994546+Jachym-VdE@users.noreply.github.com>
2026-02-19 18:19:14 +0100

    Chore/initial structure (#2)
    * fix: COC file extension typo
    
    Change the file extension of COC.MD to COC.md
    
    * feat: Add SQL models + root get
    
    Added the basic API database SQL models (user and node) and added a simple root get request for API status.
    
    * feat: add models and database init
    
    Added SQL table models and database initialization. Models added: Users, User API Keys, Nodes, and Node API Keys.
    
    * fix: fix SQL user creation errors
    
    Changed user and node models to fix SQL row and column creation errors.
    
    * feat: add user and node schemas
    
    * feat: Add pip installable package
    
    * refactor: move db files into a db folder
    
    * refactor: main.py

---
+++ src/Andromeda/api/database/init_db.py
@@ -0,0 +1,13 @@
+from sqlmodel import SQLModel, text
+from Andromeda.api.database.database import engine
+import asyncio
+
+import Andromeda.models
+
+
+print(SQLModel.metadata.tables.keys())
+
+async def init_db() -> None:
+    async with engine.begin() as conn:
+        await conn.run_sync(SQLModel.metadata.create_all)
+
--- src/Andromeda/main.py
+++ src/Andromeda/main.py
@@ -1,6 +1,22 @@
-from fastapi import FastAPI
+from contextlib import asynccontextmanager
 
-app = FastAPI()
+from fastapi import FastAPI, Depends
+from sqlmodel.ext.asyncio.session import AsyncSession
+from sqlmodel import select
+
+from Andromeda.api.database.init_db import init_db
+from Andromeda.api.database.database import engine, get_session
+from Andromeda.models.user import User, UserKey
+from Andromeda.models.node import Node, NodeKey
+from Andromeda.schemas.user import UserCreate, UserPublic
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    await init_db()
+    yield
+
+app = FastAPI(lifespan=lifespan)
 
 
 
@@ -14,3 +30,7 @@ app = FastAPI()
 # --------------- External ---------------
 # This section contains external and public functions.
 # They are intended to be publicly accessible at all times.
+
+@app.get("/")
+async def root_get():
+    return {"info":"Andromeda API is online.", "version":"v0.0.1"}
