12eb0124b7a9c7c2d452368c60faec14c9b0f61d
Jachym <jachym.vandeneynde@gmail.com>
2026-02-25 20:03:27 +0100

    Update user.py

--- src/Andromeda/schemas/user.py
+++ src/Andromeda/schemas/user.py
@@ -5,6 +5,15 @@ from pydantic import BaseModel
 from Andromeda.models.user import User, UserKey
 
 
+class UserPublic(BaseModel):
+    id: UUID
+    name: str
+    email: str
+    avatar: str
+    last_login: datetime | None
+    created_at: datetime
+
+
 class UserCreate(BaseModel):
     name: str
     email: str
@@ -15,15 +24,6 @@ class UserCreateResponse(BaseModel):
     success: bool
     message: str
     user: UserPublic
-
-
-class UserPublic(BaseModel):
-    id: UUID
-    name: str
-    email: str
-    avatar: str
-    last_login: datetime | None
-    created_at: datetime
 
 
 class UserLoginRequest(BaseModel):
